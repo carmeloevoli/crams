0d98df1d4d829cefeac29ec1b2ec54b2e3ba9c20
Carmelo Evoli <carmelo.evoli@gssi.infn.it>
2018-05-09 18:33:44 +0200

    fixed ionization losses

--- src/losses.cpp
+++ src/losses.cpp
@@ -29,16 +29,21 @@ double Losses::dE_dt_ionization(const double& E) const {
 	return v * ionization(E) * rho_0;
 }
 
-double Losses::ionization(const double& E) const {
-	double beta_ = beta(E);
-	double gamma_ = (E + proton_mass_c2) / proton_mass_c2;
-	double q_max = 2. * electron_mass_c2 * (pow2(gamma_) - 1.);
-	q_max /= 1. + 2. * gamma_ * electron_mass_c2 / proton_mass_c2 / pid.get_A();
-	double b_h = std::log(2. * electron_mass_c2 * (pow2(gamma_) - 1.) * q_max / pow2(E_H)) - 2. * pow2(beta_);
-	double b_he = std::log(2. * electron_mass_c2 * (pow2(gamma_) - 1.) * q_max / pow2(E_He)) - 2. * pow2(beta_);
+double Losses::ionization(const double& T) const {
+	double E_tot = double(pid.get_A()) * (T + proton_mass_c2);
+	double M_A = proton_mass_c2 * double(pid.get_A());
+	double p_c = std::sqrt(pow2(E_tot) - pow2(M_A));
+	double gamma_ = (T + proton_mass_c2) / proton_mass_c2;
+	double beta_ = p_c / E_tot;
 	double n_0 = rho_0 / proton_mass;
-	double value = (2. * M_PI * pow2(electron_radius) * electron_mass_c2 * n_0) * pow2(pid.get_Z()) / beta_ * (b_h + b_he * He_abundance);
-	return -value / pid.get_A() / rho_0;
+	double IsH = 19. * eV;
+	double IsHe = 44. * eV;
+	double Q_max = 2 * electron_mass_c2 * pow2(beta_) * pow2(gamma_) / (1 + 2 * gamma_ * electron_mass_c2 / M_A); // GeV
+	double B_H = std::log(2 * electron_mass_c2 * (pow2(gamma_) - 1) * Q_max / IsH / IsH) - 2 * pow2(beta_);
+	double B_He = std::log(2 * electron_mass_c2 * (pow2(gamma_) - 1) * Q_max / IsHe / IsHe) - 2 * pow2(beta_);
+	double dEdx_ion = (2 * M_PI * pow2(electron_radius) * electron_mass_c2 * n_0) * (double(pow2(pid.get_Z())) / beta_) * (B_H + B_He * He_abundance);
+	dEdx_ion /= rho_0 / double(pid.get_A());
+	return -dEdx_ion;
 }
 
 double Losses::dE_dx(const double& E) const {
@@ -47,7 +52,7 @@ double Losses::dE_dx(const double& E) const {
 
 double gslLossesClassWrapper(double x, void * pp) {
 	Losses * losses = (Losses *) pp;
-	return losses->adiabatic(x); // + losses->ionization(x); // TODO why it does not work ionization in derivative?
+	return losses->adiabatic(x) + losses->ionization(x);
 }
 
 double Losses::get_derivative(const double& E) {
@@ -56,7 +61,8 @@ double Losses::get_derivative(const double& E) {
 	gsl_function F;
 	F.function = &gslLossesClassWrapper;
 	F.params = this;
-	gsl_deriv_central (&F, E, 1e-3, &result, &abserr);
+
+	gsl_deriv_forward(&F, E, 1e-3, &result, &abserr);
 
 	return result;
 }
