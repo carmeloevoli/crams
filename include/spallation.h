41aea02fbd69f0d43c2bfb3485a5edfb61f28519
Carmelo Evoli <carmelo.evoli@gssi.it>
2019-01-17 16:30:53 +0100

    include He as a target in spallation

--- include/spallation.h
+++ include/spallation.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include "cgs.h"
 #include "pid.h"
 
 class SpallationXsecs {
@@ -14,11 +15,16 @@ public:
 	double get(const PID& projectile, const double& T) const;
 
 protected:
-	void read_table(std::string filename);
+	void read_table();
 
 protected:
 	PID _fragment;
-	std::map<PID, std::vector<double> > table;
+	std::map<PID, std::vector<double> > _table;
+	std::vector<double> _T;
+	std::string _table_filename = "xsecs_0.1_100_1.1.txt";
+	double _T_min = 0.1 * cgs::GeV;
+	size_t _T_size = 100;
+	double _T_ratio = 1.1;
 };
 
 #endif /* INCLUDE_SPALLATION_H_ */
--- src/spallation.cpp
+++ src/spallation.cpp
@@ -1,16 +1,53 @@
 #include "spallation.h"
-#include "cgs.h"
+#include "utilities.h"
 
 SpallationXsecs::SpallationXsecs(const PID& fragment) :
 		_fragment(fragment) {
+	read_table();
+	std::cout << "read " << _table.size() << " cross-sections for " << _fragment << "\n";
+
+	double T = _T_min;
+	for (size_t i = 0; i < _T_size; ++i) {
+		_T.push_back(T);
+		T *= _T_ratio;
+	}
+
 }
 
 SpallationXsecs::~SpallationXsecs() {
 }
 
 double SpallationXsecs::get(const PID& projectile, const double& T) const {
-	return 1. * cgs::mbarn;
+	double value = 0;
+	auto it = _table.find(projectile);
+	if (it != _table.end()) {
+		double T_now = std::min(T, _T.back());
+		return LinearInterpolator(_T, it->second, T_now);
+	}
+	return value * (1. + cgs::K_He * cgs::f_He) / (1. + cgs::f_He);
 }
 
-void SpallationXsecs::read_table(std::string filename) {
+void SpallationXsecs::read_table() {
+	std::ifstream inf(_table_filename.c_str());
+	if (!inf.is_open()) {
+		std::cout << "filename " << _table_filename << " cannot be open.\n";
+		exit(2);
+	} else {
+		int Z_frag, A_frag, Z_proj, A_proj;
+		double x_temp;
+		while (inf) {
+			inf >> Z_frag >> A_frag >> Z_proj >> A_proj;
+			std::vector<double> x;
+			x.reserve(100);
+			for (size_t i = 0; i < 100; ++i) {
+				inf >> x_temp;
+				x.emplace_back(x_temp * cgs::mbarn);
+			}
+			if (PID(Z_frag, A_frag) == _fragment) {
+				_table[PID(Z_proj, A_proj)] = x;
+			}
+		}
+		inf.close();
+	}
 }
+
