6f291620111674517c650972612ea4c4b44823b7
Carmelo Evoli <carmelo.evoli@gssi.it>
2019-01-19 09:03:52 +0100

    fix data dir

--- include/spallation.h
+++ include/spallation.h
@@ -21,7 +21,7 @@ protected:
 	PID _fragment;
 	std::map<PID, std::vector<double> > _table;
 	std::vector<double> _T;
-	std::string _table_filename = "xsecs_0.1_100_1.1.txt";
+	std::string _table_filename = "data/xsecs_0.1_100_1.1.txt";
 	double _T_min = 0.1 * cgs::GeV;
 	size_t _T_size = 100;
 	double _T_ratio = 1.1;
