28b0bfbfe1832f939dc4b0306edfae2fc7814a74
Igor Vaiman <gosha.vaiman@gmail.com>
2026-08-11 15:25:19 +0200

    fix for linux build

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 # TODO: make platform-independent and integrate with CMake
 
+from pathlib import Path
+
 from setuptools import Extension, setup
+
+PROJECT_ROOT = Path(__file__).parent.resolve()
 
 swig_extension = Extension(
     "crams._crams",
     sources=["extension/crams_wrap.cxx"],
-    include_dirs=["include", "external/plog/include"],
-    library_dirs=["build", "/opt/homebrew/lib"],
+    include_dirs=[str(PROJECT_ROOT / "include"), str(PROJECT_ROOT / "external/plog/include")],
+    library_dirs=[str(PROJECT_ROOT / "build"), "/opt/homebrew/lib"],
+    runtime_library_dirs=[str(PROJECT_ROOT / "build")],
     libraries=["crams_core", "gsl"],
 )
 
@@ -20,4 +25,5 @@ setup(
     packages=["crams"],
     package_dir={"crams": "extension"},
     ext_modules=[swig_extension],
+    install_requires=["numpy~=2.0"],
 )
